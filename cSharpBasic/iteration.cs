168625e8602288f01802384453f952521c8bdf2a
Aashish Bhandari <aashish.gm.bhandari1@gmail.com>
2021-07-29 20:23:28 +0545

    push

--- cSharpBasic/iteration.cs
+++ cSharpBasic/iteration.cs
@@ -1,35 +1,40 @@
 using System;
-class ItrationStatement 
+class ItrationStatement
 {
-    
-        static void Main()
-        {
-            ItrationStatement i = new ItrationStatement();
-            i.LearnForloop();
-        }
-    
+
+    static void Main()
+    {
+        ItrationStatement i = new ItrationStatement();
+        i.LearnForloop();
+    }
+
     void LearnForloop()//learning for loop 
 
-  {
     {
-      Console.WriteLine("world is beautiful");
+        {
+            Console.WriteLine("world is beautiful");
 
-    } //learning foreach loop 
-    int[] numbersa = {34,21,45,34,56,76}; 
-     
-    foreach (int x in numbersa)
+        } //learning foreach loop 
+        int[] numbersa = { 34, 21, 45, 34, 56, 76 };
 
-    {
-       if (x % 2 == 0)
-       {
-          Console.WriteLine ($"{x} is even");
+        foreach (int x in numbersa)
 
-       } 
-       else
-       {
-           Console.WriteLine($"{x} is odd");
+        {
+            if (x % 2 == 0)
+            {
+                Console.WriteLine($"{x} is even");
+
+            }
+            else
+            {
+                Console.WriteLine($"{x} is odd");
+            }
         }
-    }//learning while loop and do while loop
+
+    }
+
+}
+//learning while loop and do while loop
 //       void LearnWhileLoop()
 //       {
 //         char confirm='y';
@@ -52,5 +57,4 @@ class ItrationStatement
 //       } 
 
 //   }
-    
-// }
+
