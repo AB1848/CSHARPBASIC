e3edfb1ca44c534bcb6a3358b4d982503d649b51
Aashish Bhandari <aashish.gm.bhandari1@gmail.com>
2021-08-01 21:25:50 +0545

    linq

---
+++ LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main()
+        {
+           // LINQ -- Language Integrated Query 
+           int[] numbers = {12,14,13,18,58,65,124,95,34,65,};
+
+           var result = numbers.where(num => num > 50).Select(num => num);
+
+        }
+    }
+}
