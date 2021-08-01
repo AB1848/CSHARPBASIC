715bc8de640f20b01af64d37e21ef85e1ec18afc
Aashish Bhandari <aashish.gm.bhandari1@gmail.com>
2021-08-01 13:49:08 +0545

    new changes

---
+++ cSharpBasic/Program.cs
@@ -0,0 +1,12 @@
+public class Program
+{
+    static public void main()
+    {
+        var square1 = new Square(23.45);
+        var square2 = new Square(4);
+        var square3 = new Square(12);
+        var rectangle = new Rectangle(12.24, 456.12);
+        rectangle.PrintResult();
+    }
+}
+
--- cSharpBasic/inheritance.cs
+++ cSharpBasic/inheritance.cs
@@ -1,21 +1,5 @@
 using System;
 
-
-public class Program
-{
-    static public void main()
-    {
-        var square1 = new Square(23.45);
-        var square2 = new Square(4);
-        var square3 = new Square(12);
-        var rectangle = new Rectangle(12.24, 456.12);
-        rectangle.PrintResult();
-    }
-}
-
-
-
-
 public abstract class Shape
 {
     public abstract double GetArea();
@@ -24,7 +8,7 @@ public abstract class Shape
 
     public virtual void PrintResult()
     {
-        console.writeline("result is given as .....");
+        Console.WriteLine("result is given as .....");
 
     }
 
@@ -35,7 +19,8 @@ public class Rectangle : Shape
     public double Length { get; set; }
     public double Breadth { get; set; }
 
-    public RecData(double length, double breadth)
+    public Rectangle(double length,
+                   double breadth)
     {
         Length = length;
         Breadth = breadth;
@@ -43,19 +28,21 @@ public class Rectangle : Shape
 
 
     public override double GetArea() => Length * Breadth;
-    public override double GetCircumgference() => 2 * (Length * Breadth);
+    public override double GetCircumference() => 2 * (Length * Breadth);
 
     public override void PrintResult()
     {
         Console.WriteLine($"the area and the circumference of rectangle [{Length }x {Breadth}]");
-        Console.writeline($"area:{this.GetArea()} Circumference:{this.GetCircumference()}");
+        Console.WriteLine($"area:{this.GetArea()} Circumference:{this.GetCircumference()}");
     }
 
 }
 public class Square : Rectangle
 {
-    public GetValue(double side): base(side,side)
-    {}
+    public Square(double side): base (side,side)
+    {
+        
+    }
 
 }
 public class circle : Shape
